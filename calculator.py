01cf8d5fb1ca6e40d4c998be668e29f8321b7b8d
farwa <farwa.creates@gmail.com>
2026-07-21 17:33:57 +0530

    Add files via upload

--- calculator.py
+++ calculator.py
@@ -1,4 +1,6 @@
-x=float(input("whats x?"))
-y=float(input("whats y?"))
-z=(x/y)
-print(f"{z:.2f}")
+def main():
+    x = int(input("what is x? "))
+    print("x cubed is", cube(x))
+def cube(n):
+    return pow(n, 3)
+main()
